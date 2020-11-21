8674c1ad32c40be3ecfc9985caa14789d820b939
Michael Stephens <mdstephens26@yahoo.com>
2020-11-21 17:48:24 -0500

    commit tree 66490278448904e2f86115979f2188f670803bf7

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,24 @@
 # Write your code here.
+
+def line(deli)
+    if deli.empty?
+        puts "The line is currently empty."
+      else
+        names = ""
+        deli.each_with_index {|name, index| names.concat " #{index.next}. #{name}" }
+        puts "The line is currently:#{names}"
+      end
+    end
+    def take_a_number deli, newPerson
+     deli << newPerson
+     puts "Welcome, #{newPerson}. You are number #{deli.length} in line."
+     #deli.length
+    end
+    def now_serving deli
+     if deli.empty?
+       puts "There is nobody waiting to be served!"
+     else
+       name = deli.shift
+       puts "Currently serving #{name}."
+     end
+    end
